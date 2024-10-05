eb19bc49231578191236a45cd5a7658f5eebdfc9
tomshmueli <tomshmueli@mail.tau.ac.il>
2024-10-05 15:53:43 +0300

    delete test flop

--- long_term_forecast_l720/exp/exp_main.py
+++ long_term_forecast_l720/exp/exp_main.py
@@ -336,9 +336,6 @@ class Exp_Main(Exp_Basic):
                     pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
                     visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
 
-        if self.args.test_flop:
-            test_params_flop((batch_x.shape[1],batch_x.shape[2]))
-            exit()
         preds = np.array(preds)
         trues = np.array(trues)
         inputx = np.array(inputx)
